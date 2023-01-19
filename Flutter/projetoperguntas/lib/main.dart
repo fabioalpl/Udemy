24fb2d98cf2177d7de731969424b866ad070ab9d
Fabio de Almeida <fabioalmeida@prodam.sp.gov.br>
2023-01-19 11:22:51 -0300

    Até a aula 57.

--- Flutter/projetoperguntas/lib/main.dart
+++ Flutter/projetoperguntas/lib/main.dart
@@ -16,14 +16,32 @@ class PerguntasApp extends StatefulWidget {
 }
 
 class _PerguntasAppState extends State<PerguntasApp> {
+  //Lista de Map
+  final _perguntas = const [
+    {
+      'texto': "Qual é a sua cor favorita?",
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Maria', 'João', 'Leo', 'Pedro'],
+    }
+  ];
+
   var _perguntaSelecionada = 0;
 
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
 
-    print("Pergunta respondida");
+    //print("Pergunta respondida");
   }
 
   void Function() funcaoQueRetornaOutraFuncao() {
@@ -32,32 +50,55 @@ class _PerguntasAppState extends State<PerguntasApp> {
     };
   }
 
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final perguntas = [
+    // Array
+    /*final perguntas = [
       "Qual é a sua cor favorita?",
       "Qual é o seu animal preferido?"
-    ];
+    ];*/
+
+    /*for (var textResp in perguntas[_perguntaSelecionada].cast()['respostas']) {
+      //print(textResp);
+      respostas.add(Resposta(textResp, _responder));
+    }*/
+
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()['resposta']
+        : [];
+    List<Widget> widgets =
+        respostas.map((t) => Resposta(t, _responder)).toList();
 
     return Scaffold(
       appBar: AppBar(
         title: Text("Perguntas"),
       ),
-      body: Column(
-        children: [
-          //Text(perguntas.elementAt(_perguntaSelecionada)),
-          Questao(perguntas[_perguntaSelecionada]),
-          Resposta("Resposta 1", _responder),
+      body: temPerguntaSelecionada
+          ? Column(
+              children: [
+                //Text(perguntas.elementAt(_perguntaSelecionada)),
+                Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                ...widgets,
+                /*Resposta("Resposta 1", _responder),
           Resposta("Resposta 2", _responder),
-          Resposta("Resposta 3", _responder),
-          //ElevatedButton(onPressed: _responder, child: Text("Resposta 1")),
-          /*ElevatedButton(
+          Resposta("Resposta 3", _responder),*/
+                //ElevatedButton(onPressed: _responder, child: Text("Resposta 1")),
+                /*ElevatedButton(
               onPressed: () => print("Resposta 2"), child: Text("Resposta 2")),
           ElevatedButton(
               onPressed: funcaoQueRetornaOutraFuncao,
               child: Text("Resposta 3")),*/
-        ],
-      ),
+              ],
+            )
+          : Center(
+              child: Text(
+              "Parabéns!",
+              style: TextStyle(fontSize: 28),
+            )),
     );
   }
 }
